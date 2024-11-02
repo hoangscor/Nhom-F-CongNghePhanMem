b3d366cc27e2d096399775191da8055167293df6
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2024-11-02 21:22:00 +0700

    add full

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using HairHarmonySalon.Models;
+
+namespace HairHarmonySalon.Controllers
+{
+	public class ServicesController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+        public IActionResult Appointment()
+        {
+            return View();
+        }
+        public IActionResult PickTime()
+        {
+            return View();
+        }
+        
+        public IActionResult SelectStylist()
+        {
+            return View();
+        }
+       
+    }
+}
